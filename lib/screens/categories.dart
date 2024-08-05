63a2b3a7ac894d11d2cd146283e7b055897240e0
hitalo <hitalo.2014.hs@gmail.com>
2024-08-05 16:53:48 -0300

    feat: screen navigation implemented on categories screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectedCategory(BuildContext context) {
+    // Navigator.of(context).push(route);    // Alternative way
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (ctx) =>
+                const MealsScreen(title: "Category here", meals: [])));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +32,12 @@ class CategoriesScreen extends StatelessWidget {
             ),
             children: [
               for (final category in availableCategories)
-                CategoryGridItem(category: category)
+                CategoryGridItem(
+                  category: category,
+                  onSelectCategory: () {
+                    _selectedCategory(context);
+                  },
+                )
             ]),
       ),
     );
