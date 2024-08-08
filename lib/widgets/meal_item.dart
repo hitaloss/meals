85fe6debd1f0161e3adaf2e3b1ac2534467dbe2f
hitalo <hitalo.2014.hs@gmail.com>
2024-08-07 23:02:22 -0300

    refactor: added specific meal screen redirect plus added semantic label on pictures for accessibility

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/screens/meal_details.dart';
 import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
@@ -18,6 +19,11 @@ class MealItem extends StatelessWidget {
         meal.affordability.name.substring(1);
   }
 
+  void _selectedMeal(BuildContext context) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (ctx) => MealDetailsScreen(meal: meal)));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -26,7 +32,9 @@ class MealItem extends StatelessWidget {
       clipBehavior: Clip.hardEdge,
       elevation: 2,
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          _selectedMeal(context);
+        },
         child: Stack(children: [
           FadeInImage(
             placeholder: MemoryImage(kTransparentImage),
@@ -34,6 +42,7 @@ class MealItem extends StatelessWidget {
             fit: BoxFit.cover,
             height: 200,
             width: double.infinity,
+            imageSemanticLabel: meal.title,
           ),
           Positioned(
               bottom: 0,
