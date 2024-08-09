8c9d2ebd9290d9b54db61ae80f422fd52c7fc1cc
hitalo <hitalo.2014.hs@gmail.com>
2024-08-09 17:19:33 -0300

    feat:

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
-import 'package:meals/screens/meal_details.dart';
 import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealItem extends StatelessWidget {
-  const MealItem({super.key, required this.meal});
+  const MealItem({super.key, required this.meal, required this.onSelectMeal});
 
   final Meal meal;
+  final void Function(BuildContext context, Meal meal) onSelectMeal;
 
   String get complexityText {
     return meal.complexity.name[0].toUpperCase() +
@@ -19,11 +19,6 @@ class MealItem extends StatelessWidget {
         meal.affordability.name.substring(1);
   }
 
-  void _selectedMeal(BuildContext context) {
-    Navigator.push(context,
-        MaterialPageRoute(builder: (ctx) => MealDetailsScreen(meal: meal)));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -33,7 +28,7 @@ class MealItem extends StatelessWidget {
       elevation: 2,
       child: InkWell(
         onTap: () {
-          _selectedMeal(context);
+          onSelectMeal(context, meal);
         },
         child: Stack(children: [
           FadeInImage(
