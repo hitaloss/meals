7b80e918f028e446484ccc9e7caa7b7553dc4062
hitalo <hitalo.2014.hs@gmail.com>
2024-08-19 18:10:59 -0300

    feat: applying data to pop from settings page

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:meals/widgets/switch_tile_filter.dart';
 
+enum Filter {
+  glutenFree,
+  lactoseFree,
+  vegetarian,
+  vegan,
+}
+
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
 
@@ -28,38 +35,66 @@ class _FiltersScreenState extends State<FiltersScreen> {
       appBar: AppBar(
         title: const Text("Your Filters"),
       ),
-      body: Column(
-        children: [
-          SwitchTileFilter(
-            glutenFreeFilterSet: _filters['gluten']!,
-            changePropertyStatus: (isChecked) =>
-                _changeFilterStatus('gluten', isChecked),
-            title: "Gluten-free",
-            subtitle: "Only include gluten-free meals.",
-          ),
-          SwitchTileFilter(
-            glutenFreeFilterSet: _filters['lactose']!,
-            changePropertyStatus: (isChecked) =>
-                _changeFilterStatus('lactose', isChecked),
-            title: "Lactose-free",
-            subtitle: "Only include lactose-free meals.",
-          ),
-          SwitchTileFilter(
-            glutenFreeFilterSet: _filters['vegetarian']!,
-            changePropertyStatus: (isChecked) =>
-                _changeFilterStatus('vegetarian', isChecked),
-            title: "Vegetarian",
-            subtitle: "Only include vegetarian meals.",
-          ),
-          SwitchTileFilter(
-            glutenFreeFilterSet: _filters['vegan']!,
-            changePropertyStatus: (isChecked) =>
-                _changeFilterStatus('vegan', isChecked),
-            title: "Vegan",
-            subtitle: "Only include vegan meals.",
-          ),
-        ],
+      body: PopScope(
+        canPop: false,
+        onPopInvoked: (bool didPop) {
+          if (didPop) return;
+          Navigator.of(context).pop({
+            Filter.glutenFree: _filters['gluten'],
+            Filter.lactoseFree: _filters['lactose'],
+            Filter.vegetarian: _filters['vegetarian'],
+            Filter.vegan: _filters['vegan'],
+          });
+        },
+        child: Column(
+          children: [
+            SwitchTileFilter(
+              glutenFreeFilterSet: _filters['gluten']!,
+              changePropertyStatus: (isChecked) =>
+                  _changeFilterStatus('gluten', isChecked),
+              title: "Gluten-free",
+              subtitle: "Only include gluten-free meals.",
+            ),
+            SwitchTileFilter(
+              glutenFreeFilterSet: _filters['lactose']!,
+              changePropertyStatus: (isChecked) =>
+                  _changeFilterStatus('lactose', isChecked),
+              title: "Lactose-free",
+              subtitle: "Only include lactose-free meals.",
+            ),
+            SwitchTileFilter(
+              glutenFreeFilterSet: _filters['vegetarian']!,
+              changePropertyStatus: (isChecked) =>
+                  _changeFilterStatus('vegetarian', isChecked),
+              title: "Vegetarian",
+              subtitle: "Only include vegetarian meals.",
+            ),
+            SwitchTileFilter(
+              glutenFreeFilterSet: _filters['vegan']!,
+              changePropertyStatus: (isChecked) =>
+                  _changeFilterStatus('vegan', isChecked),
+              title: "Vegan",
+              subtitle: "Only include vegan meals.",
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+/**
+ * PopScope(
+  canPop: false,
+  onPopInvoked: (bool didPop) {
+    if(didPop) return;
+    Navigator.of(context).pop({
+      Filter.glutenFree: _glutenFreeFilterSet,
+      Filter.lactoseFree: _lactoseFreeFilterSet,
+      Filter.vegetarian: _vegetarianFilterSet,
+      Filter.vegan: _veganFilterSet,
+    });
+  },
+  child: Column(...) // same code as shown in the next lecture
+),
+ */
