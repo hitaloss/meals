cc127df4d27c2211c270813a8531418139c74110
hitalo <hitalo.2014.hs@gmail.com>
2024-09-16 20:18:57 -0300

    refactor: applied favorites provider, along with improve code verbosity

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
-// import 'package:meals/data/dummy_data.dart';
+
+import 'package:meals/providers/meals_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
-import 'package:meals/models/meal.dart';
 import 'package:meals/widgets/main_drawer.dart';
+import 'package:meals/providers/favorites_provider.dart';
 
 const kInitialFilters = {
   Filter.glutenFree: false,
@@ -14,22 +16,21 @@ const kInitialFilters = {
   Filter.vegan: false,
 };
 
-class TabsScreen extends StatefulWidget {
+class TabsScreen extends ConsumerStatefulWidget {
   const TabsScreen({super.key});
 
   @override
-  State<TabsScreen> createState() => _TabsScreenState();
+  ConsumerState<TabsScreen> createState() => _TabsScreenState();
 }
 
-class _TabsScreenState extends State<TabsScreen> {
+class _TabsScreenState extends ConsumerState<TabsScreen> {
   int _selectedPageIndex = 0;
-  final List<Meal> _favoriteMeals = [];
-  // Map<Filter, bool> _selectedFilters = {
-  //   Filter.glutenFree: false,
-  //   Filter.lactoseFree: false,
-  //   Filter.vegetarian: false,
-  //   Filter.vegan: false,
-  // };
+  Map<Filter, bool> _selectedFilters = {
+    Filter.glutenFree: false,
+    Filter.lactoseFree: false,
+    Filter.vegetarian: false,
+    Filter.vegan: false,
+  };
 
   void _selectPage(int index) {
     setState(() {
@@ -37,67 +38,50 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
-  void _toggleMealFavoriteStatus(Meal meal) {
-    if (_favoriteMeals.contains(meal)) {
-      setState(() {
-        _favoriteMeals.remove(meal);
-      });
-      _showInfoMessage("Meal unmarked as favorite.");
-    } else {
-      setState(() {
-        _favoriteMeals.add(meal);
-      });
-      _showInfoMessage("Marked as favorite!");
-    }
-  }
-
-  void _showInfoMessage(String message) {
-    ScaffoldMessenger.of(context).clearSnackBars();
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        duration: const Duration(milliseconds: 1500), content: Text(message)));
-  }
-
   void _setScreen(String identifier) async {
     Navigator.pop(context);
     if (identifier == 'filters') {
       // Navigator.pushReplacement(
       //     context, MaterialPageRoute(builder: (ctx) => const FiltersScreen()));
-      /*final result = await */ Navigator.push<Map<Filter, bool>>(
-          context, MaterialPageRoute(builder: (ctx) => const FiltersScreen()));
+      final result = await Navigator.push<Map<Filter, bool>>(
+          context,
+          MaterialPageRoute(
+              builder: (ctx) =>
+                  FiltersScreen(currentFilters: _selectedFilters)));
 
-      // setState(() {
-      //   _selectedFilters = result ?? kInitialFilters;
-      // });
+      setState(() {
+        _selectedFilters = result ?? kInitialFilters;
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    // final availableMeals = dummyMeals.where((meal) {
-    //   if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
-    //     return false;
-    //   }
-    //   if (_selectedFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
-    //     return false;
-    //   }
-    //   if (_selectedFilters[Filter.vegetarian]! && !meal.isVegetarian) {
-    //     return false;
-    //   }
-    //   if (_selectedFilters[Filter.vegan]! && !meal.isVegan) {
-    //     return false;
-    //   }
-    //   return true;
-    // }).toList();
+    final meals = ref.watch(mealsProvider);
 
-    Widget activePage = CategoriesScreen(
-      onToggleFavorite: _toggleMealFavoriteStatus,
-    );
+    final availableMeals = meals.where((meal) {
+      if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+        return false;
+      }
+      if (_selectedFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+        return false;
+      }
+      if (_selectedFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+        return false;
+      }
+      if (_selectedFilters[Filter.vegan]! && !meal.isVegan) {
+        return false;
+      }
+      return true;
+    }).toList();
+
+    Widget activePage = CategoriesScreen(availableMeals: availableMeals);
     var activePageTitle = "Categories";
 
     if (_selectedPageIndex == 1) {
+      final favoriteMeals = ref.watch(favoriteMealsProvider);
       activePage = MealsScreen(
-        meals: _favoriteMeals,
-        onToggleFavorite: _toggleMealFavoriteStatus,
+        meals: favoriteMeals,
       );
       activePageTitle = "Your Favorites";
     }
