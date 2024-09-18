634e57716b10c417165b8d5853fad4890cc2fca2
hitalo <hitalo.2014.hs@gmail.com>
2024-09-18 18:25:04 -0300

    refactor: state management rework made with riverpod providers

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,35 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'package:meals/widgets/switch_tile_filter.dart';
 import 'package:meals/providers/filters_provider.dart';
 
-class FiltersScreen extends ConsumerStatefulWidget {
+class FiltersScreen extends StatelessWidget {
   const FiltersScreen({super.key});
-
-  @override
-  ConsumerState<FiltersScreen> createState() => _FiltersScreenState();
-}
-
-class _FiltersScreenState extends ConsumerState<FiltersScreen> {
-  final Map<String, bool> _filters = {};
-
-  @override
-  void initState() {
-    super.initState();
-    final activeFilters = ref.read(filtersProvider);
-    _filters['gluten'] = activeFilters[Filter.glutenFree]!;
-    _filters['lactose'] = activeFilters[Filter.lactoseFree]!;
-    _filters['vegetarian'] = activeFilters[Filter.vegetarian]!;
-    _filters['vegan'] = activeFilters[Filter.vegan]!;
-  }
-
-  void _changeFilterStatus(String filter, bool isChecked) {
-    setState(() {
-      _filters[filter] = isChecked;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,48 +12,29 @@ class _FiltersScreenState extends ConsumerState<FiltersScreen> {
       appBar: AppBar(
         title: const Text("Your Filters"),
       ),
-      body: PopScope(
-        canPop: true,
-        onPopInvoked: (bool didPop) {
-          ref.read(filtersProvider.notifier).setFilters({
-            Filter.glutenFree: _filters['gluten']!,
-            Filter.lactoseFree: _filters['lactose']!,
-            Filter.vegetarian: _filters['vegetarian']!,
-            Filter.vegan: _filters['vegan']!,
-          });
-        },
-        child: Column(
-          children: [
-            SwitchTileFilter(
-              propertyStatusFilterSet: _filters['gluten']!,
-              changePropertyStatus: (isChecked) =>
-                  _changeFilterStatus('gluten', isChecked),
-              title: "Gluten-free",
-              subtitle: "Only include gluten-free meals.",
-            ),
-            SwitchTileFilter(
-              propertyStatusFilterSet: _filters['lactose']!,
-              changePropertyStatus: (isChecked) =>
-                  _changeFilterStatus('lactose', isChecked),
-              title: "Lactose-free",
-              subtitle: "Only include lactose-free meals.",
-            ),
-            SwitchTileFilter(
-              propertyStatusFilterSet: _filters['vegetarian']!,
-              changePropertyStatus: (isChecked) =>
-                  _changeFilterStatus('vegetarian', isChecked),
-              title: "Vegetarian",
-              subtitle: "Only include vegetarian meals.",
-            ),
-            SwitchTileFilter(
-              propertyStatusFilterSet: _filters['vegan']!,
-              changePropertyStatus: (isChecked) =>
-                  _changeFilterStatus('vegan', isChecked),
-              title: "Vegan",
-              subtitle: "Only include vegan meals.",
-            ),
-          ],
-        ),
+      body: const Column(
+        children: [
+          SwitchTileFilter(
+            filter: Filter.glutenFree,
+            title: "Gluten-free",
+            subtitle: "Only include gluten-free meals.",
+          ),
+          SwitchTileFilter(
+            filter: Filter.lactoseFree,
+            title: "Lactose-free",
+            subtitle: "Only include lactose-free meals.",
+          ),
+          SwitchTileFilter(
+            filter: Filter.vegetarian,
+            title: "Vegetarian",
+            subtitle: "Only include vegetarian meals.",
+          ),
+          SwitchTileFilter(
+            filter: Filter.vegan,
+            title: "Vegan",
+            subtitle: "Only include vegan meals.",
+          ),
+        ],
       ),
     );
   }
--- lib/widgets/switch_tile_filter.dart
+++ lib/widgets/switch_tile_filter.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
 
-class SwitchTileFilter extends StatelessWidget {
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:meals/providers/filters_provider.dart';
+
+class SwitchTileFilter extends ConsumerWidget {
   const SwitchTileFilter({
     super.key,
-    required this.propertyStatusFilterSet,
-    required this.changePropertyStatus,
     required this.title,
     required this.subtitle,
+    required this.filter,
   });
 
-  final bool propertyStatusFilterSet;
-  final void Function(bool isChecked) changePropertyStatus;
   final String title;
   final String subtitle;
+  final Filter filter;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final activeFilters = ref.watch(filtersProvider);
+
     return SwitchListTile(
-      value: propertyStatusFilterSet,
+      value: activeFilters[filter]!,
       onChanged: (isChecked) {
-        changePropertyStatus(isChecked);
+        ref.read(filtersProvider.notifier).setFilter(filter, isChecked);
       },
       title: Text(
         title,
