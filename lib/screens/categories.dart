e33d17e4c20b9f67944267810866015a07874122
hitalo <hitalo.2014.hs@gmail.com>
2024-09-25 00:16:06 -0300

    refactor: applied more smooth animation, along with implementing curved animation

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -24,7 +24,7 @@ class _CategoriesScreenState extends State<CategoriesScreen>
 
     _animationController = AnimationController(
       vsync: this,
-      duration: const Duration(milliseconds: 300),
+      duration: const Duration(milliseconds: 700),
       lowerBound: 0,
       upperBound: 1,
     );
@@ -54,27 +54,30 @@ class _CategoriesScreenState extends State<CategoriesScreen>
   @override
   Widget build(BuildContext context) {
     return AnimatedBuilder(
-        animation: _animationController,
-        child: GridView(
-            padding: const EdgeInsets.all(24),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20,
-            ),
-            children: [
-              for (final category in availableCategories)
-                CategoryGridItem(
-                  category: category,
-                  onSelectCategory: () {
-                    _selectedCategory(context, category);
-                  },
-                )
-            ]),
-        builder: (context, child) => Padding(
-            padding:
-                EdgeInsets.only(top: 100 - _animationController.value * 100),
-            child: child));
+      animation: _animationController,
+      child: GridView(
+          padding: const EdgeInsets.all(24),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          children: [
+            for (final category in availableCategories)
+              CategoryGridItem(
+                category: category,
+                onSelectCategory: () {
+                  _selectedCategory(context, category);
+                },
+              )
+          ]),
+      builder: (context, child) => SlideTransition(
+        position: Tween(begin: const Offset(0, 0.3), end: const Offset(0, 0))
+            .animate(CurvedAnimation(
+                parent: _animationController, curve: Curves.easeInOut)),
+        child: child,
+      ),
+    );
   }
 }
